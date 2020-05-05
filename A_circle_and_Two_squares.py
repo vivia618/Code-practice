53e4b5042aee3d0ab63454644034c18771d0a46e
vivia618 <vivia618@hotmail.com>
2020-05-05 09:57:46 +0200

    Add practice A circle and two squares

---
+++ A_circle_and_Two_squares.py
@@ -0,0 +1,12 @@
+# Imagine a circle and two squares: a smaller and a bigger one.
+# For the smaller one, the circle is a circumcircle and for the bigger one,
+# an incircle.
+
+# Create a function, that takes an integer (radius of the circle)
+# and returns the square area of the square inside the circle.
+
+def square_areas_difference(x):
+    return 2 * x**2
+
+
+print(square_areas_difference(7))
